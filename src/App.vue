52cb2a3a1901472d378419af542a3c9c1112449e
Aster0N <nikvas131@gmail.com>
2022-10-19 22:26:44 +0300

    Added color-picker component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<template></template>
+<template>
+	<color-picker></color-picker>
+</template>
 
 <script>
+import ColorPicker from "@/components/ColorPicker.vue";
+
 export default {
 	name: "App",
+	components: {
+		ColorPicker,
+	},
 };
 </script>
